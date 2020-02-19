fd4c4b9912ce3640f06a70ff63dbae3d5fc00138
Sharan777 <sharangouda021@gmail.com>
2020-02-19 18:49:52 +0530

    1 file modified, 16 added

---
+++ src/app/compare/compare-results/compare-results.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-compare-results',
+  templateUrl: './compare-results.component.html',
+  styleUrls: ['./compare-results.component.css']
+})
+export class CompareResultsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
