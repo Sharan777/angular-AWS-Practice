084adc2c319d9b7a7e8bd7b7e52bc8e90af080b0
Sharan777 <sharangouda021@gmail.com>
2020-02-20 16:37:53 +0530

    basic sigin-signup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from './user/auth.service';
 
 @Component({
   selector: 'my-app',
@@ -6,5 +9,27 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+  isAuthenticated = false;
+
+  constructor(private authService: AuthService,
+              private router: Router) {
+  }
+
+  ngOnInit() {
+    this.authService.authStatusChanged.subscribe(
+      (authenticated) => {
+        this.isAuthenticated = authenticated;
+        if (authenticated) {
+          this.router.navigate(['/compare']);
+        } else {
+          this.router.navigate(['/']);
+        }
+      }
+    );
+    this.authService.initAuth();
+  }
+
+  onLogout() {
+    this.authService.logout();
+  }
 }
