8381ec204414bd4c67573257bc144dd353671977
Sharan777 <sharangouda021@gmail.com>
2020-02-20 15:33:39 +0530

    1 file modified

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subject } from 'rxjs/Subject';
+
+import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
+import {
+  CognitoUserPool,
+  CognitoUserAttribute,
+  CognitoUser,
+  AuthenticationDetails,
+  CognitoUserSession
+} from 'amazon-cognito-identity-js';
+
+import { User } from './user.model';
 
 @Injectable()
 export class AuthService {
