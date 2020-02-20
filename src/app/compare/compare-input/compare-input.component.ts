2aaaa039b7778728cdb2119a272af34d85e632c7
Sharan777 <sharangouda021@gmail.com>
2020-02-20 16:51:08 +0530

    6 files modified added all data with AWS post get delete test

--- src/app/compare/compare-input/compare-input.component.ts
+++ src/app/compare/compare-input/compare-input.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { CompareData } from '../compare-data.model';
+import { NgForm } from '@angular/forms';
+
+import { CompareService } from '../compare.service';
 
 @Component({
   selector: 'app-compare-input',
@@ -6,10 +10,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./compare-input.component.css']
 })
 export class CompareInputComponent implements OnInit {
+  
+  @ViewChild('compareForm') form: NgForm;
+  isLoading = false;
+  couldNotLoadData = false;
 
-  constructor() { }
+  constructor(private compareService: CompareService) {
+  }
 
   ngOnInit() {
+    this.compareService.dataIsLoading.subscribe(
+      (isLoading: boolean) => this.isLoading = isLoading
+    );
+    this.compareService.dataLoadFailed.subscribe(
+      (didFail: boolean) => {
+        this.couldNotLoadData = didFail;
+        this.isLoading = false;
+      }
+    );
+  }
+
+  onSubmit() {
+    const data: CompareData = {
+      age: this.form.value.age as number,
+      height: this.form.value.height as number,
+      income: this.form.value.income as number
+    };
+    this.compareService.onStoreData(data);
+  }
+
+  onFetchStoredData() {
+    this.compareService.onRetrieveData(false);
   }
 
 }
